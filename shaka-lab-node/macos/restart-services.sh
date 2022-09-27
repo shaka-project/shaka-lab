51e1a0f6e3349ca081b891503a4b05c4d7baf5d3
Joey Parrish <joeyparrish@google.com>
2022-09-26 20:32:12 -0700

    Fix service restart

--- shaka-lab-node/macos/restart-services.sh
+++ shaka-lab-node/macos/restart-services.sh
@@ -5,18 +5,23 @@ INSTALL_PATH="$(dirname "$0")"
 restart_service() {
   local NAME="$1"
 
-  # Load the service if it's not loaded yet.
+  # Link the service into the user's LaunchAgent folder, so that it is
+  # automatically loaded on login.
+  ln -sf "$INSTALL_PATH/$NAME.plist" ~/Library/LaunchAgents/
+
+  # Load the service now if it's not loaded yet.
   local LIST="$(launchctl list | grep "$NAME")"
   if [ -z "$LIST" ]; then
     echo "Loading $NAME"
-    launchctl load "$INSTALL_PATH/$NAME.plist"
+    launchctl load ~/Library/LaunchAgents/"$NAME.plist"
   fi
 
   # Stop and start the service.
   echo "Restarting $NAME"
   launchctl stop "$NAME"
-  launchctl start "$INSTALL_PATH/$NAME.plist"
+  launchctl start "$NAME"
 }
 
+# Restart both services.
 restart_service shaka-lab-node-update
 restart_service shaka-lab-node-service
